49615a1d6cac85fe378e22dbf347a87c7b54a98c
Kunal Tapkir <kunal.tapkir@synerzip.com>
2020-02-01 20:40:38 +0530

    api user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route GET api/users/test
-// @desc Tests users route
+// @route POST api/users/test
+// @desc Register user
 // @access Public
-router.get('/test', (req, res) => res.json({ msg: "Users Works" }));
+router.post('/', [
+  check('name', 'Name is required')
+    .not()
+    .isEmpty(),
+  check('email', 'Pleases include valid email').isEmail(),
+  check('password',
+    'Please enter a password with 6 or more characters')
+    .isLength({ min: 6 })
+],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send({ msg: "Users route" })
+  });
 
 module.exports = router;
